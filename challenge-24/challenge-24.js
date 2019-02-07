809c76d14e69379b7435026286369395010ffd13
Vinícius Araújo <32021203+viniciuswz@users.noreply.github.com>
2019-02-07 21:50:32 -0200

    ok

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -10,66 +10,157 @@ listeners de eventos, etc);
 mesma funcionalidade.
 */
 
-var $visor = document.querySelector('[data-js="visor"]');
-var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
-var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]');
-var $buttonCE = document.querySelector('[data-js="button-ce"]');
-var $buttonEqual = document.querySelector('[data-js="button-equal"]');
 
-Array.prototype.forEach.call($buttonsNumbers, function(button) {
-  button.addEventListener('click', handleClickNumber, false);
-});
-Array.prototype.forEach.call($buttonsOperations, function(button) {
-  button.addEventListener('click', handleClickOperation, false);
-});
-$buttonCE.addEventListener('click', handleClickCE, false);
-$buttonEqual.addEventListener('click', handleClickEqual, false);
-
-function handleClickNumber() {
-  $visor.value += this.value;
-}
-
-function handleClickOperation() {
-  $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  $visor.value += this.value;
-}
-
-function handleClickCE() {
-  $visor.value = 0;
-}
-
-function isLastItemAnOperation(number) {
-  var operations = ['+', '-', 'x', '÷'];
-  var lastItem = number.split('').pop();
-  return operations.some(function(operator) {
-    return operator === lastItem;
-  });
-}
-
-function removeLastItemIfItIsAnOperator(number) {
-  if(isLastItemAnOperation(number)) {
-    return number.slice(0, -1);
-  }
-  return number;
-}
-
-function handleClickEqual() {
-  $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  var allValues = $visor.value.match(/\d+[+x÷-]?/g);
-  $visor.value = allValues.reduce(function(accumulated, actual) {
-    var firstValue = accumulated.slice(0, -1);
-    var operator = accumulated.split('').pop();
-    var lastValue = removeLastItemIfItIsAnOperator(actual);
-    var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
-    switch(operator) {
-      case '+':
-        return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
-      case '-':
-        return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-      case 'x':
-        return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
-      case '÷':
-        return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
+(function(win,doc){
+  'use strict'
+    var operation = {
+        '+': function(x,y){
+            return (Number(x)) + (Number(y));
+        },
+        '-': function(x,y){
+            return (Number(x)) - (Number(y));
+        },
+        '*': function(x,y){
+            return (Number(x)) * (Number(y));
+        },
+        '/': function(x,y){
+            return (Number(x)) / (Number(y));
+        },
+        '%': function(x,y){
+            return (Number(x)) % (Number(y));
+        }
     }
-  });
-}
+   
+    var $buttonData = document.querySelectorAll('[data-js]');
+    var $button = document.querySelectorAll('button');
+    var $visor = doc.querySelector('[type="text"]')
+    //var $reg = /\d+([\+\*\/\-])\d+/g;
+    var $reg = /\-?[\d\.]+([\+\*\/\-]).?[\d\.]+/;
+    var regInit = /\-?[\d\.]+([\*\/]).?[\d\.]+/;
+   // var $button = document.querySelector('[data-js="1"]');
+    
+
+    console.log($buttonData)
+    function myFunction(event) {
+        event.preventDefault();
+        var visorValue = $visor.value;
+        initCalculator.call(this,null,visorValue);
+    }
+
+    function initCalculator($this,valorInitial){
+        var $this = this.value;
+
+        //verifyLastDigit($this);
+        //console.log(valorInitial); 
+        if(valorInitial == 0 && $this != '>')
+           return $visor.value =  $this; 
+        else if($this == 'apagar')
+            return $visor.value = 0;
+        else if($this == '=')
+            return $visor.value = sendCalc(valorInitial);
+        else if(isSignal($this))
+            return verifyLastDigit($this,valorInitial);
+        else if($this == '>')
+            deleteLastDigit($this);
+        else
+           return $visor.value +=  $this
+    }
+
+    function verifyLastDigit(signal,valorInitial){
+        var lastSignalDigit = valorInitial.substring(valorInitial.length - 1);
+
+        if(lastSignalDigit == signal)
+            return $visor.value = sendCalc(valorInitial.substring(0,valorInitial.length - 1));
+        else if(isSignal(lastSignalDigit))
+            return $visor.value = valorInitial.substring(0,valorInitial.length - 1) + signal;
+        else
+            return $visor.value = valorInitial + signal
+
+        //console.log(signal)
+    }
+
+    function deleteLastDigit($this){
+        var $length = $visor.value.length;
+        if($length >1)
+            return $visor.value = $visor.value.substring(0, $length - 1)
+        else if($length > 1 || $this == '>')
+            return $visor.value = 0
+    }
+
+    function isSignal($this){
+       return $this == '+' || $this == '-' || $this == '*' || $this == '/'
+    }
+    
+    //console.log(sum(1,2,3,4,5,6))
+
+   
+    function sendCalc(visor){
+        //var initSignal = /[\*\/]/
+        //var normalSignal = /\d([\+\-])/
+        if(verifySignal(regInit,visor)){
+            return makeCalc(regInit,visor)
+        }else{
+            return makeCalc($reg,visor)
+        }
+        
+    }
+
+    function verifySignal(reg,visor){
+        var bacon = reg.test(visor);
+        if(bacon){
+            return bacon;
+        }
+    }
+
+    function makeCalc(reg,visor){
+       return makeCalcAgain(calcs(reg,removeLastSignal(visor)))
+    }
+
+    function removeLastSignal(visor){
+        
+        if(isSignal(visor.slice(-1)))
+        return visor.slice(0,-1)
+        else
+            return visor;
+    }
+
+    function calcs(reg,calc){
+    return calc.replace(reg,function(index,signal){
+            var values = index.match(/^([\+\-]?[\d\.]+)|([\d\.]+)/g);
+            values.push(signal);
+            //return mult.apply(mult,index.match(/\d+/g));
+            //return verifyCalc.apply(verifyCalc,sigal,index.match(/\d+/g));
+            return verifyCalc.apply(verifyCalc,values);
+        })
+    }
+
+    function makeCalcAgain(calc){
+        if(/.?\d[\+\*\/\-]/g.test(calc))
+            return sendCalc(calc)
+        else
+            return calc
+    }
+
+
+    function verifyCalc(x,y,signal){
+        return operation[signal](x,y);
+    }
+
+
+
+    //console.log(verifyCalc('*',1,2))
+
+    // console.log(sendCalc('10+1*2*3*4'))
+
+    // $button[0].addEventListener('click',myFunction,false)
+
+    Array.prototype.forEach.call($buttonData,function($this){
+        console.log($this.addEventListener('click',myFunction,false))
+    })
+0
+    Array.prototype.forEach.call($button,function($this){
+        console.log($this.addEventListener('click',myFunction,false))
+    })
+ 
+  
+})(window,document)
